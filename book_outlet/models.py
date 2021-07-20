3d4e743898c0723b9eb302854e2d329c45fdaa89
Ubuntu <ubuntu@ip-172-31-30-172.ap-south-1.compute.internal>
2021-07-20 13:18:24 +0000

    module 80 completed Migration updating and deleting data from db

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 
 class Book(models.Model):
     title = models.CharField(max_length=60)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False) 
+
+    def __str__(self) -> str:
+        return f"{self.title}({self.rating})"
