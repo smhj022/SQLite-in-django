f85d510e002b6a0cb77b491d5f9a12ae8ecadde0
Ubuntu <ubuntu@ip-172-31-30-172.ap-south-1.compute.internal>
2021-07-21 06:09:24 +0000

    section 6 completed - models, filters, overwrite, query

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,7 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
+
 # Create your models here.
 
 
@@ -8,7 +10,17 @@ class Book(models.Model):
     rating = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
-    is_bestselling = models.BooleanField(default=False) 
+    is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True) 
+     # bd_index is used because slug is used in url so to increase the performance db_index is se to True
 
+
+
+    # show queryset in the form defined
     def __str__(self) -> str:
         return f"{self.title}({self.rating})"
+
+    # overwrite method, here we overwriting save method to slugify over title(Harry Potter 1-->harry-potter-1)
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Book
+from django.http import Http404
+from django.db.models import Avg
 
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all()
+    no_of_book = books.count()
+    avg_rating = books.aggregate(Avg('rating'))
+    print(avg_rating)
+    return render(request, "book_outlets/index.html", {
+        "books": books,
+        "total_no_of_books": no_of_book,
+        "average_rating": avg_rating
+    })
+
+
+def book_details(request, slug):
+    try:
+        book = Book.objects.get(slug=slug)
+        return render(request, "book_outlets/book_details.html", {
+            "book": book,
+        })
+    except:
+        raise Http404
