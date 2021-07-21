e1dee616bf7c16dfce28919edc35da38d0b8f78e
Ubuntu <ubuntu@ip-172-31-30-172.ap-south-1.compute.internal>
2021-07-21 12:19:06 +0000

    one to many relationship between book and author

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Author, Book
+
 # Register your models here.
 
 
@@ -11,3 +12,5 @@ class BookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+
+admin.site.register(Author)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,18 +5,29 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
 class Book(models.Model):
     title = models.CharField(max_length=60)
     rating = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    # One to many relationship between author and book / on_delete is used to clarify what happen when author is deleted
+    # CASCADE delete book also
+    # PROTECT avoide delete book data
+    # SET_NULL set to null
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True) 
-     # bd_index is used because slug is used in url so to increase the performance db_index is se to True
-
-
+    slug = models.SlugField(default="", null=False, db_index=True)
+    # bd_index is used because slug is used in url so to increase the performance db_index is se to True
 
     # show queryset in the form defined
+
     def __str__(self) -> str:
         return f"{self.title}({self.rating})"
 
