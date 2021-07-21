b26461c3d53b2d6b2fa4de06e8dffdecfe3a7f77
Ubuntu <ubuntu@ip-172-31-30-172.ap-south-1.compute.internal>
2021-07-21 18:57:25 +0000

    relationships done

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Author, Book
+from .models import Author, Book, Address, Country
 
 # Register your models here.
 
@@ -14,3 +14,7 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)
 
 admin.site.register(Author)
+
+admin.site.register(Address)
+
+admin.site.register(Country)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,10 +4,30 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+# create many to many relationship with book
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=2)
+
+       def __str__(self) -> str:
+        return f"{self.name} {self.code}"
+
+
+
+# create one to one relationship with Author
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=6)
+    city = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.street}, {self.postal_code}, {self.city}"
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
@@ -25,6 +45,7 @@ class Book(models.Model):
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True)
     # bd_index is used because slug is used in url so to increase the performance db_index is se to True
+    published_countries = models.ManyToManyField(Country)
 
     # show queryset in the form defined
 
